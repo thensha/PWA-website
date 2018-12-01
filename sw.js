b6a97c9e44bfe198a0e5f73f2b52d8c065b4a2ec
wy <wy_1407@outlook.com>
2018-12-01 19:13:33 +0800

    init

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const VERSION = 'v2';
+'use strict'
+
+const VERSION = 'v5';
 
 //安装后立刻激活并执行serviceWorker
 self.addEventListener('install', ev => {
@@ -41,23 +43,31 @@ self.addEventListener('activate', function (event) {
 
 
 self.addEventListener('fetch', function (event) {
-
     event.respondWith(
         caches.match(event.request)
         .then(function (response) {
             if (response) {
                 return response;
             }
+
+            // 因为 event.request 流已经在 caches.match 中使用过一次，
+            // 那么该流是不能再次使用的。我们只能得到它的副本，拿去使用。
             var fetchRequest = event.request.clone();
 
+            // fetch 的通过信方式，得到 Request 对象，然后发送请求
             return fetch(fetchRequest).then(
                 function (response) {
-                    if (!response || response.status !== 200) {
+                    // 检查是否成功
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
                         return response;
                     }
 
+                    // 如果成功，该 response 一是要拿给浏览器渲染，而是要进行缓存。
+                    // 不过需要记住，由于 caches.put 使用的是文件的响应流，一旦使用，
+                    // 那么返回的 response 就无法访问造成失败，所以，这里需要复制一份。
                     var responseToCache = response.clone();
-                    caches.open(cacheName)
+
+                    caches.open(VERSION)
                         .then(function (cache) {
                             cache.put(event.request, responseToCache);
                         });
@@ -71,5 +81,4 @@ self.addEventListener('fetch', function (event) {
             });
         })
     );
-
 });
